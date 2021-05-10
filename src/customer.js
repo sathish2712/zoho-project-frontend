495d0985961d4b9dbe3e1dbe97e78f0c550918e1
sathish2712 <sathish27121998@gmail.com>
2021-05-10 16:38:53 +0530

    customer_function

--- src/customer.js
+++ src/customer.js
@@ -1,15 +1,35 @@
 const cname = document.querySelector("#name")
+const Gender = document.querySelector("#gender")
+const Phone = document.querySelector("#phone")
+const State = document.querySelector("#state")
+const City = document.querySelector("#city")
+const Product = document.querySelector("#products")
+const paymentType = document.querySelector("#payment")
+const paymentDetails = document.querySelector("#pin")
+
+const mgs = document.querySelector("#mgs")
 const formSubmit = document.querySelector("#formData")
+
+mgs.textContent = ''
 formSubmit.addEventListener('click', function(e) {
     e.preventDefault()
     const custName = cname.value
+    const gender = Gender.value
+    const phone = Phone.value
+    const state = Phone.value
+    const city = City.value
+    const prod = Product.value
+    const ptype = paymentType.value
+    const pdetails = paymentDetails.value
     console.log("test")
-    fetch('http://localhost:8080/backend-Zoho/webapi/store/customer?name=' + custName, {
+    fetch('http://localhost:8080/backend-Zoho/webapi/store/customer?name=' + custName + "&gender=" + gender + "&phone=" + phone + "&state=" + state + "&city=" + city + "&products=" + product + "&product=" + prod + "&ptype=" + ptype + "&pdetail=" + pdetails, {
         method: 'post',
         headers: {
             "Content-type": "application/json"
         },
     }).then(function(response) {
-        console.log(response)
+        if (response.status == 200) {
+            mgs.textContent = 'Your invoice has been created (locally) '
+        }
     })
 })
